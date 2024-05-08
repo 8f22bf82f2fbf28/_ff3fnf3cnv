42f7ccf60b155106d6253c6184e27f440a8576a6
8f22bf82f2fbf28 <van11nebackup@gmail.com>
2024-05-08 11:44:30 -0500

    Update AntiDebugger.lua

--- Files/AntiDebugger.lua
+++ Files/AntiDebugger.lua
@@ -1,7 +1,7 @@
 local CoreGui = game:GetService("CoreGui")
 
 game:GetService("RunService").RenderStepped:Connect(function()
-	if game:GetService("Players").LocalPlayer.Name ~= "nopitching" or "DontMovy" then
+	if game:GetService("Players").LocalPlayer.Name ~= "nopitching" then
 		for _, GUI in pairs(CoreGui:GetChildren()) do
 			if GUI:FindFirstChild("ImageLabel") then
 				game:Shutdown()
