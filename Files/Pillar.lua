efb82ea61df7a8e07f72cd5886233a0af42bcaec
8f22bf82f2fbf28 <van11nebackup@gmail.com>
2024-06-29 18:59:49 -0500

    Create Pillar.lua

---
+++ Files/Pillar.lua
@@ -0,0 +1,90 @@
+local RunService = game:GetService("RunService")
+local TeleportService = game:GetService("TeleportService")
+local TweenService = game:GetService("TweenService")
+
+local Games = {
+	Shinden = {
+		Lobby = 6808589067,
+		Main = 10369535604,
+	},
+}
+
+local repo = 'https://raw.githubusercontent.com/violin-suzutsuki/LinoriaLib/main/'
+
+local Library = loadstring(game:HttpGet(repo .. 'Library.lua'))()
+local ThemeManager = loadstring(game:HttpGet(repo .. 'addons/ThemeManager.lua'))()
+local SaveManager = loadstring(game:HttpGet(repo .. 'addons/SaveManager.lua'))()
+local Sense = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Sirius/request/library/sense/source.lua'))()
+
+local Players = game:GetService("Players")
+
+local Player = Players.LocalPlayer
+local Character = Player.Character or Player.CharacterAdded:Wait()
+
+function onCharacterAdded(character)
+	Character = character
+
+	if game.PlaceId == 16560655288 then
+		
+	end
+end
+
+Player.CharacterAdded:Connect(onCharacterAdded)
+
+local Window = Library:CreateWindow({
+	Title = tostring("Tear - " .. game.PlaceId),
+	Center = true,
+	AutoShow = true,
+	TabPadding = 8,
+	MenuFadeTime = 0.2
+})
+
+local Tabs = {
+	Main = Window:AddTab('Main'),
+	ESP = Window:AddTab('ESP'),
+	['UI Settings'] = Window:AddTab('UI Settings'),
+}
+
+getgenv().test = false
+
+local Cheats = Tabs.Main:AddLeftGroupbox('')
+local Cheats2 = Tabs.Main:AddRightGroupbox('')
+
+
+Cheats:AddToggle('Test', {
+	Text = 'Test',
+	Default = false,
+	Tooltip = "test button",
+	Callback = function(Value)
+		
+	end
+})
+Library:SetWatermarkVisibility(false)
+
+Library.KeybindFrame.Visible = false;
+
+Library:OnUnload(function()
+	Library.Unloaded = true
+end)
+
+local MenuGroup = Tabs['UI Settings']:AddLeftGroupbox('Menu')
+
+MenuGroup:AddButton('Unload', function() Library:Unload() end)
+MenuGroup:AddLabel('Menu bind'):AddKeyPicker('MenuKeybind', { Default = 'KeypadEight', NoUI = true, Text = 'Menu keybind' })
+
+Library.ToggleKeybind = Options.MenuKeybind
+
+ThemeManager:SetLibrary(Library)
+SaveManager:SetLibrary(Library)
+
+SaveManager:IgnoreThemeSettings()
+
+SaveManager:SetIgnoreIndexes({ 'MenuKeybind' })
+
+SaveManager:SetFolder('Tear/specific-game')
+
+SaveManager:BuildConfigSection(Tabs['UI Settings'])
+
+ThemeManager:ApplyToTab(Tabs['UI Settings'])
+
+SaveManager:LoadAutoloadConfig()
